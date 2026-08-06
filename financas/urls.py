09bea0807bfb920a2d40b10eaf17076ae3be5099
Pedro Augusto <pedroaug23@outlook.com>
2026-08-06 15:12:42 -0300

    feat: criação do dashboard

--- financas/urls.py
+++ financas/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import CategoriaListCreateView, TransacaoListCreateView
+from .views import CategoriaListCreateView, CategoriaDetailView, TransacaoListCreateView, DashboardView
 
 urlpatterns = [
     path('categorias/', CategoriaListCreateView.as_view(), name='categoria-list'),
+    path('categorias/<int:pk>/', CategoriaDetailView.as_view(), name='categoria-detail'),
     path('transacoes/', TransacaoListCreateView.as_view(), name='transacao-list'),
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),
 ]
 
--- financas/views.py
+++ financas/views.py
@@ -2,6 +2,9 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Categoria, Transacao
 from .serializers import CategoriaSerializer, TransacaoSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.db.models import Sum
 
 class CategoriaListCreateView(generics.ListCreateAPIView):
     serializer_class = CategoriaSerializer
@@ -13,6 +16,13 @@ class CategoriaListCreateView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(usuario=self.request.user)
 
+class CategoriaDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = CategoriaSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Categoria.objects.filter(usuario=self.request.user)
+
 class TransacaoListCreateView(generics.ListCreateAPIView):
     serializer_class = TransacaoSerializer
     permission_classes = [IsAuthenticated]
@@ -22,3 +32,21 @@ class TransacaoListCreateView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(usuario=self.request.user)
+
+class DashboardView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        transacoes = Transacao.objects.filter(usuario=request.user)
+
+        receitas = transacoes.filter(categoria__tipo='receita').aggregate(total=Sum('valor'))['total'] or 0.00
+
+        despesas = transacoes.filter(categoria__tipo='despesa').aggregate(total=Sum('valor'))['total'] or 0.00
+
+        saldo = float(receitas) - float(despesas)
+
+        return Response({
+            "total_receitas": round(float(receitas), 2),
+            "total_despesas": round(float(despesas), 2),
+            "saldo_atual": round(saldo, 2)
+        })
